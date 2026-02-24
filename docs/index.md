36532fc91965c13042fa53716c1c8ae44eacb509
smileluck <12386904+smileluck@users.noreply.github.com>
2026-02-24 16:37:19 +0800

    feat: 添加 Giscus 评论插件并更新 GitHub 链接
    - 添加 rspress-plugin-giscus 依赖以支持评论功能
    - 更新 GitHub 仓库链接至新地址
    - 配置 Giscus 插件参数

--- docs/index.md
+++ docs/index.md
@@ -11,7 +11,7 @@ hero:
       link: /guide/
     - theme: alt
       text: GitHub
-      link: https://github.com/web-infra-dev/rspress
+      link: https://github.com/smileluck/SmileX-Note-Repress
   image:
     src: /rspress-icon.png
     alt: Logo
--- rspress.config.ts
+++ rspress.config.ts
@@ -1,5 +1,6 @@
 import * as path from 'node:path';
 import { defineConfig } from '@rspress/core';
+import { pluginGiscus } from 'rspress-plugin-giscus'; // 或 rspress-plugin-code-giscus
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
@@ -14,8 +15,15 @@ export default defineConfig({
       {
         icon: 'github',
         mode: 'link',
-        content: 'https://github.com/web-infra-dev/rspress',
+        content: 'https://github.com/smileluck/SmileX-Note-Repress',
       },
     ],
   },
+  plugins: [pluginGiscus({
+    repo: 'smileluck/smilex-Note-Repress',
+    repoId: 'R_kgDORXWUpA',
+    category: 'General',
+    categoryId: 'DIC_kwDORXWUpM4C3GT9',
+    lang: 'zh-CN',
+  })],
 });
